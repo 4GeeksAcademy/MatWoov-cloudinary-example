bad63bb90c03b84115420e94dea905878042417d
Mathew Woo <133248984+MatWoov@users.noreply.github.com>
2023-11-18 12:39:07 +0000

    feat: cloudinary example

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,17 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+class UserImage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(255), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Image %r>' % self.id
+    
+    def serialize(self):
+        return {
+            "url": self.url,
+            "id": self.id
+        }
